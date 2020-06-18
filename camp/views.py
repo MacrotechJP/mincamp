0c726fc6e1a3fa688e4448553581b8a0fb20c066
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-06-18 12:17:54 +0900

    検索画面ルーティング追加

--- camp/views.py
+++ camp/views.py
@@ -5,3 +5,6 @@ from basicauth.decorators import basic_auth_required
 @basic_auth_required
 def index(request):
    return render(request, 'camp/index.html')
+
+def search(request):
+   return render(request, 'camp/search.html')
