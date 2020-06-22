98b6575862b6f03488d6f02aebf1adce4295be07
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-06-22 22:23:02 +0900

    新規登録（メール認証付き）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# app_name = 'accounts'
-
-# urlpatterns = [
-#     path('signup/', views.signup, name='signup'),
-#     path('signin/', views.signin, name='signin'),
-# ]
-
 from django.urls import path
 from . import views
 
@@ -15,6 +5,10 @@ app_name = 'accounts'
 
 urlpatterns = [
     # path('', views.Top.as_view(), name='top'),
+    path('', views.Login.as_view(), name='sign_in'),
     path('sign_in/', views.Login.as_view(), name='sign_in'),
     path('sign_out/', views.Logout.as_view(), name='sign_out'),
+    path('sign_up/', views.UserCreate.as_view(), name='sign_up'),
+    path('sign_up/done', views.UserCreateDone.as_view(), name='sign_up_done'),
+    path('sign_up/complete/<token>/', views.UserCreateComplete.as_view(), name='sign_up_complete'),
 ]
--- mincamp/settings.py
+++ mincamp/settings.py
@@ -22,6 +22,18 @@ BASICAUTH_USERS={env('BASIC_USER'):env('BASIC_PASS')}
 # カスタムユーザモデル
 AUTH_USER_MODEL = 'accounts.User'
 
+# メールをコンソールに表示する
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# メールサーバーへの接続設定
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tanakakoyo1999@gmail.com'
+# EMAIL_HOST_USER = 'tanakakoyo@macrotech.jp'
+EMAIL_HOST_PASSWORD = 'ltdnhkphhbnzoewj'
+# EMAIL_HOST_PASSWORD = 'jpgwipplcoaprxvy'
+EMAIL_USE_TLS = True
+
 
 ALLOWED_HOSTS = ['*']
 
