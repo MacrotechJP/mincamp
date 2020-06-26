6029e9d95e47dde8384ff5359be70d7ef80cd9cb
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-06-27 02:47:28 +0900

    微修正

--- mincamp/urls.py
+++ mincamp/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path('', include('camp.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('social_django.urls', namespace='social')),
+    path('accounts/', include('social_django.urls', namespace='social')),   # ソーシャルログイン関連
     path('admin/', admin.site.urls),
     path('mypage/', include('mypage.urls')),
-
-    # path('oauth/', include('social_django.urls', namespace='social')),
 ]
