6040da89698211f624c26aabeb491e6aba47866a
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-06-27 02:40:11 +0900

    認証機能追加

--- mincamp/urls.py
+++ mincamp/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', include('camp.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('social_django.urls', namespace='social')),
     path('admin/', admin.site.urls),
     path('mypage/', include('mypage.urls')),
+
+    # path('oauth/', include('social_django.urls', namespace='social')),
 ]
