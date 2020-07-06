137d65e25e6e26d369161eb15b4994855edfe72a
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-07-07 01:41:29 +0900

    ホスト詳細ページモック作成途中

---
+++ static/js/camp/detail.js
@@ -0,0 +1,12 @@
+$(function(){
+    /**
+     * 画像スライドショー
+     * ポップアップ画面
+     */
+    $(".host-images").iziModal({
+        group: "group01",
+        loop: true,
+        fullscreen: true,
+        headerColor: "#91b500",
+    });
+})
