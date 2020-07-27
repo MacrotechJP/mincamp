1e1047c5824fe09813bc8cb90340619fb32dd107
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-07-28 01:14:54 +0900

    ホスト詳細ページモック作成途中

--- static/js/camp/detail.js
+++ static/js/camp/detail.js
@@ -19,5 +19,24 @@ $(function(){
      * 用具レンタル
      * 画像スライドショー
      */
+
+
+    /** 
+     * プロフィールアニメーション
+     */
+    // This just toggles the follow/following of the button
+$('a.follow').click(function () {
+    $(this).toggleClass('followed');
+    
+    if($(this).hasClass('followed')) {
+      $(this).text('Followed');
+      $('ul li:last-child').html('325<span>Followers</span>');
+    }
+    else {
+      $(this).text('Follow Nick');
+      $('ul li:last-child').html('324<span>Followers</span>');
+    }
+  });
+      
     
 })
