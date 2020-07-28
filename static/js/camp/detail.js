65325e9c784eb7c63e2d783abc4c3ff7615678a3
KoyoTanaka <tanakakoyo@macrotech.jp>
2020-07-28 20:24:51 +0900

    ホスト詳細ページモック作成途中

--- static/js/camp/detail.js
+++ static/js/camp/detail.js
@@ -37,6 +37,55 @@ $('a.follow').click(function () {
       $('ul li:last-child').html('324<span>Followers</span>');
     }
   });
+    
+  
+    /**
+     * 周辺のアクティビティー
+     */
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+    $(function () {
+        $(".material-card > .mc-btn-action").click(function () {
+          var card = $(this).parent(".material-card");
+          var icon = $(this).children("i");
+          icon.addClass("fa-spin-fast");
       
+          if (card.hasClass("mc-active")) {
+            card.removeClass("mc-active");
+      
+            window.setTimeout(function () {
+              icon
+                .removeClass("fa-arrow-left")
+                .removeClass("fa-spin-fast")
+                .addClass("fa-bars");
+            }, 800);
+          } else {
+            card.addClass("mc-active");
+      
+            window.setTimeout(function () {
+              icon
+                .removeClass("fa-bars")
+                .removeClass("fa-spin-fast")
+                .addClass("fa-arrow-left");
+            }, 800);
+          }
+        });
+      });
+      
+    
     
 })
