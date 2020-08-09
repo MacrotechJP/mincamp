ec8ddeacf7562185b651925bf8e68623507ce3c2
TanakaKoyo <tanakakoyo@macrotech.jp>
2020-08-09 19:28:48 +0900

    予約ページ(1/3)モック完了

--- static/js/camp/reservation_apply.js
+++ static/js/camp/reservation_apply.js
@@ -1,7 +1,21 @@
 $(function(){
+
+
+
+
+
     /**
-     * 予約プログレスバー
+     * 料金の内訳
      */
+    $('#price-breakdown').iziModal({
+        title: '料金の内訳',
+        fullscreen: true,
+        headerColor: "#91b500",
+    });
+    $(".contents_apply_host_info_breakdown span").on("click",function(){
+        $('#price-breakdown').iziModal('open');
+    })
+
     
 
 
